c095951b95804f5dd5f26a05219ba7e88d053a74
sanjaynou <sanjay.adhek@gmail.com>
2023-09-17 07:32:43 +0530

    First commit

---
+++ First Program/firstProgram.js
@@ -0,0 +1,26 @@
+// Date Display
+var date = Date();
+//console.log(date)
+//document.getElementById('demo').innerHTML = date
+
+
+//click Me function
+
+function clickMe(){
+    //Change Date
+    /*document.getElementById('demo').innerHTML = date;*/
+
+    //Change HTML Content
+    /*document.getElementById('demo').innerHTML = "I'am fine now."*/
+
+    // Change HTML Style
+    /*document.getElementById('demo').style.fontSize="20px"*/
+
+    //Change Hide HTML Element
+    /*document.getElementById('demo').style.display="none"*/
+
+    //Change Display HTML Element
+    document.getElementById('demo').style.display="block"
+
+
+}
