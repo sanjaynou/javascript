eafc56d9ac91c81b943b5914b85bc3d347ea0875
sanjaynou <sanjay.adhek@gmail.com>
2023-09-23 18:51:08 +0530

    Variable Update

---
+++ Variables/index.js
@@ -0,0 +1,39 @@
+/*
+Variables are Containers for Storing Data
+There are 4 types of variables
+1.Automatically
+2.Using Var (Gobal Variable)
+3.Using let
+4.Using const
+*/
+
+//Example-1
+x = 5;
+y = 6;
+z = x + y;
+//document.getElementById('demo').innerHTML = z;
+
+//Example-2
+var a = 5;
+var b = 10;
+var c = a + b;
+//document.getElementById('demo').innerHTML = c;
+
+//Example-3
+let p = 10;
+let q = 20
+let r = p + q
+//document.getElementById('demo').innerHTML = r;
+
+//Example-4
+const d = 20
+const e = 25
+const f = d + e
+//document.getElementById('demo') .innerHTML = f;
+
+//Example-5(Mixed) and JavaScript Identifiers
+const firstAmount = 100
+const secondAmount = 200
+let GrandTotal = firstAmount + secondAmount
+//document.getElementById('demo').innerHTML = GrandTotal
+
